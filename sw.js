400e1bf370cda9d3210e3c81d0d1b015e01fff2d
Mimic27 <matteogogogo92@gmail.com>
2026-04-10 18:09:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,32 @@
 const CACHE = 'guide-citoyen-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+// Utilisation de ./ pour que GitHub Pages trouve les fichiers dans le dossier
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(r => {
+      return r || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
   );
 });
